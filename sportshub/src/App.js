3e4c5cec3cbdfe1af9c2b1917d6cef9e3f954f31
MurtazaBagwala07 <murtazabag786@gmail.com>
2022-07-17 21:07:28 +0530

    initial setup

---
+++ sportshub/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { Routes,Route } from "react-router-dom";
+import Mockman from "mockman-js";
+import {Home,Profile,SignUp,SignIn} from './pages'
+
+
+function MockAPI() {
+  return (
+    <div className="MockAPI">
+      <Mockman />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/mockman' element={<MockAPI/>}></Route>
+        <Route path='/profile/:user' element={<Profile/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/signin' element={<SignIn/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ sportshub/src/pages/index.js
@@ -0,0 +1,4 @@
+export {Home} from './Home/Home'
+export {Profile} from './Profile/Profile'
+export {SignIn} from './SignIn/SignIn'
+export {SignUp} from './SignUp/SignUp'
